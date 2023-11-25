9f41761c531e29d8df2f0847c26c7f72556c88c1
Ale-Campos <alejandrocampos9092@gmail.com>
2023-11-25 17:54:42 -0300

    Arreglo en Edit de Cursos

--- src/app/dashboard/pages/courses/components/courses-dialog/courses-dialog.component.ts
+++ src/app/dashboard/pages/courses/components/courses-dialog/courses-dialog.component.ts
@@ -6,27 +6,32 @@ import { Course } from 'src/data/Courses';
 @Component({
   selector: 'app-courses-dialog',
   templateUrl: './courses-dialog.component.html',
-  styleUrls: ['./courses-dialog.component.scss']
+  styleUrls: ['./courses-dialog.component.scss'],
 })
 export class CoursesDialogComponent {
   courseForm: FormGroup;
-  constructor(private formBuilder: FormBuilder,
+  constructor(
+    private formBuilder: FormBuilder,
     private matDialogRef: MatDialogRef<CoursesDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data?:Course
-    ) {
-   this.courseForm = this.formBuilder.group({
-    name: ["", [Validators.required]],
-    startDate: ["", [Validators.required]],
-    endDate: ["", [Validators.required]]
-   });
+    @Inject(MAT_DIALOG_DATA) public data?: Course
+  ) {
+    this.courseForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      startDate: ['', [Validators.required]],
+      endDate: ['', [Validators.required]],
+    });
 
-   if(this.data) {
-    this.courseForm.patchValue(this.data)
-   }
+    if (this.data) {  
+      this.courseForm.patchValue({
+        name: this.data.name,
+        startDate: new Date(this.data.startDate),
+        endDate: new Date(this.data.endDate),
+      });
+    }
   }
 
   onSubmit(): void {
-    if(this.courseForm.invalid) {
+    if (this.courseForm.invalid) {
       this.courseForm.markAllAsTouched();
     } else {
       this.matDialogRef.close(this.courseForm.value);
